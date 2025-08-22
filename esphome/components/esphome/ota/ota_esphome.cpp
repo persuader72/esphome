6c9d69e32b59d1af2e0c8c9ca3a64f7c45d80efb
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-08-22 12:37:02 +0000

    ota: patch to make it works with meshmesh

--- esphome/components/esphome/ota/ota_esphome.cpp
+++ esphome/components/esphome/ota/ota_esphome.cpp
@@ -8,6 +8,9 @@
 #include "esphome/components/ota/ota_backend_arduino_libretiny.h"
 #include "esphome/components/ota/ota_backend_arduino_rp2040.h"
 #include "esphome/components/ota/ota_backend_esp_idf.h"
+#ifdef USE_MESH_MESH
+#include "esphome/components/meshmesh/meshmesh.h"
+#endif
 #include "esphome/core/application.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/log.h"
@@ -473,7 +476,7 @@ bool ESPHomeOTAComponent::writeall_(const uint8_t *buf, size_t len) {
   return true;
 }
 
-float ESPHomeOTAComponent::get_setup_priority() const { return setup_priority::AFTER_WIFI; }
+float ESPHomeOTAComponent::get_setup_priority() const { return setup_priority::LATE; }
 uint16_t ESPHomeOTAComponent::get_port() const { return this->port_; }
 void ESPHomeOTAComponent::set_port(uint16_t port) { this->port_ = port; }
 
@@ -495,6 +498,9 @@ void ESPHomeOTAComponent::cleanup_connection_() {
 }
 
 void ESPHomeOTAComponent::yield_and_feed_watchdog_() {
+#ifdef USE_MESH_MESH
+  meshmesh::MeshmeshComponent::getInstance()->loop();
+#endif
   App.feed_wdt();
   delay(1);
 }
