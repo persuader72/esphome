e12b5f729b37664752d0c9c65ef8aeb25ca9073b
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-08-24 08:15:41 +0200

    Partial revert of: "meshmesh: added a platoform ota component"

--- esphome/__main__.py
+++ esphome/__main__.py
@@ -416,17 +416,14 @@ def upload_program(config: ConfigType, args: ArgsProtocol, host: str) -> int | s
         return 1  # Unknown target platform
 
     ota_conf = {}
-
-    if CONF_ESPHOME not in args.ota_platform:
-        args.ota_platform.append(CONF_ESPHOME)
     for ota_item in config.get(CONF_OTA, []):
-        if ota_item[CONF_PLATFORM] in args.ota_platform:
+        if ota_item[CONF_PLATFORM] == CONF_ESPHOME:
             ota_conf = ota_item
-        break
+            break
 
     if not ota_conf:
         raise EsphomeError(
-            f"Cannot upload Over the Air as the {CONF_OTA} configuration is not present or does not include {CONF_PLATFORM}: {args.ota_platform}"
+            f"Cannot upload Over the Air as the {CONF_OTA} configuration is not present or does not include {CONF_PLATFORM}: {CONF_ESPHOME}"
         )
 
     from esphome import espota2
@@ -1017,13 +1014,6 @@ def parse_args(argv):
         action="store_true",
         help="Reset the device before starting serial logs.",
         default=os.getenv("ESPHOME_SERIAL_LOGGING_RESET"),
-    )
-
-    parser_run.add_argument(
-        "--ota-platform",
-        action="append",
-        help="Manually specify the platform to use for OTA updates.",
-        default=["esphome"],
     )
 
     parser_clean = subparsers.add_parser(
