e1c376fd7964b1135804a0931742ac9e349d6361
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-08-26 19:50:06 +0000

    first working send automation

--- esphome/components/meshmesh_direct/automation.h
+++ esphome/components/meshmesh_direct/automation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "meshmesh_direct.h"
+#include "esphome/components/meshmesh/meshmesh.h"
 
 #include "esphome/core/automation.h"
 #include "esphome/core/base_automation.h"
@@ -13,8 +14,14 @@ template<typename... Ts> class SendAction : public Action<Ts...>, public Parente
   TEMPLATABLE_VALUE(uint32_t, address);
   TEMPLATABLE_VALUE(std::vector<uint8_t>, data);
 
-  void play(Ts... x) override {
-    ESP_LOGI("meshmesh_direct", "Sending data to %d", this->address_.value(x...));
+  void play_complex(Ts... x) override {
+    uint32_t address = this->address_.value(x...);
+    std::vector<uint8_t> data = this->data_.value(x...);
+    ESP_LOGI("meshmesh_direct", "Sending data %d to %d", data.size(), address);
+    this->parent_->meshmesh()->uniCastSendData(data.data(), data.size(), address);
+  }
+
+  void play(Ts... x) override { /* ignore - see play_complex */
   }
 
 }; // class SendAction
